fbecee16651ffab170ea4fe0b0fe9d65587cc5e8
Yoann Rodière <yoann@hibernate.org>
2023-12-18 12:52:36 +0100

    Restructure fetching code
    Mainly to allow for easier parallelized fetching.

--- src/main/java/io/quarkus/search/app/util/GitCloneDirectory.java
+++ src/main/java/io/quarkus/search/app/util/GitCloneDirectory.java
@@ -2,20 +2,10 @@ package io.quarkus.search.app.util;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.net.URI;
-import java.util.List;
-
-import io.quarkus.search.app.entity.Language;
-import io.quarkus.search.app.quarkusio.QuarkusIO;
-
-import io.quarkus.logging.Log;
 
 import org.hibernate.search.util.common.impl.Closer;
-import org.hibernate.search.util.common.impl.SuppressingCloser;
 
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.eclipse.jgit.revwalk.RevTree;
 
 public class GitCloneDirectory implements Closeable {
@@ -23,36 +13,9 @@ public class GitCloneDirectory implements Closeable {
 
     private final CloseableDirectory directory;
     private final String pagesBranch;
-    protected RevTree pagesTree;
+    private RevTree pagesTree;
 
-    public static GitCloneDirectory mainRepository(URI gitUri, List<String> branches)
-            throws IOException, GitAPIException {
-        return create(gitUri, branches, "quarkus.io", QuarkusIO.PAGES_BRANCH);
-    }
-
-    public static GitCloneDirectory localizedRepository(Language language, URI gitUri, List<String> branches) {
-        return create(gitUri, branches, language.code + ".quarkus.io", QuarkusIO.LOCALIZED_PAGES_BRANCH);
-    }
-
-    private static GitCloneDirectory create(URI gitUri, List<String> branches, String prefix, String branch) {
-        Log.infof("Fetching from %s.", gitUri);
-        CloseableDirectory directory = null;
-        Git git = null;
-        try {
-            directory = CloseableDirectory.temp(prefix);
-            git = createGitClone(gitUri, branches, directory);
-
-            return new GitCloneDirectory(git, directory, branch);
-        } catch (IOException | GitAPIException e) {
-            new SuppressingCloser(e)
-                    .pushAll(git)
-                    .pushAll(directory);
-            throw new IllegalStateException(
-                    "Failed to clone git repository '" + gitUri + "': " + e.getMessage(), e);
-        }
-    }
-
-    private GitCloneDirectory(Git git, CloseableDirectory directory, String pagesBranch) {
+    public GitCloneDirectory(Git git, CloseableDirectory directory, String pagesBranch) {
         this.git = git;
         this.directory = directory;
         this.pagesBranch = pagesBranch;
@@ -85,17 +48,4 @@ public class GitCloneDirectory implements Closeable {
         }
     }
 
-    private static Git createGitClone(URI gitUri, List<String> branches, CloseableDirectory directory)
-            throws GitAPIException {
-        return Git.cloneRepository()
-                .setURI(gitUri.toString())
-                .setDirectory(directory.path().toFile())
-                .setDepth(1)
-                .setNoTags()
-                .setBranch(branches.get(0))
-                .setBranchesToClone(branches.stream().map(b -> "refs/heads/" + b).toList())
-                .setProgressMonitor(new TextProgressMonitor())
-                // Unfortunately sparse checkouts are not supported: https://www.eclipse.org/forums/index.php/t/1094825/
-                .call();
-    }
 }
