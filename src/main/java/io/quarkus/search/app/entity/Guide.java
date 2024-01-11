8c03ea5d708b563a28a1a6c016a27c9daf1cbaef
Yoann Rodière <yoann@hibernate.org>
2024-01-11 11:15:49 +0100

    Make Guide.toString() work better with markdown

--- src/main/java/io/quarkus/search/app/entity/Guide.java
+++ src/main/java/io/quarkus/search/app/entity/Guide.java
@@ -114,7 +114,7 @@ public class Guide {
     @Override
     public String toString() {
         return "Guide{" +
-                "url='" + url + '\'' +
+                "url=<" + url + '>' +
                 '}';
     }
 }
