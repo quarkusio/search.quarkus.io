1c7f8ef89b835b75c581d241aaba9b13c55ff70b
Yoann Rodière <yoann@hibernate.org>
2024-01-17 15:58:15 +0100

    Return an approximate total hit count above a given threshold

--- src/main/java/io/quarkus/search/app/dto/SearchResult.java
+++ src/main/java/io/quarkus/search/app/dto/SearchResult.java
@@ -2,5 +2,13 @@ package io.quarkus.search.app.dto;
 
 import java.util.List;
 
-public record SearchResult<T>(long total, List<T> hits) {
+public record SearchResult<T>(Total total, List<T> hits) {
+    public SearchResult(org.hibernate.search.engine.search.query.SearchResult<T> result) {
+        this(new Total(result.total().isHitCountExact() ? result.total().hitCount() : null,
+                result.total().hitCountLowerBound()),
+                result.hits());
+    }
+
+    public record Total(Long exact, Long lowerBound) {
+    }
 }
