6eadf835b104926a83f9e699fe7141c0766a0681
marko-bekhta <marko.prykladna@gmail.com>
2024-02-15 14:05:48 +0100

    Different grouping of analyzer configs

--- src/main/java/io/quarkus/search/app/hibernate/AnalysisConfigurer.java
+++ src/main/java/io/quarkus/search/app/hibernate/AnalysisConfigurer.java
@@ -1,6 +1,5 @@
 package io.quarkus.search.app.hibernate;
 
-import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.regex.Pattern;
 
@@ -74,153 +73,188 @@ public class AnalysisConfigurer implements ElasticsearchAnalysisConfigurer {
         EnumSet<Language> englishLanguages = EnumSet.of(Language.ENGLISH, Language.PORTUGUESE, Language.SPANISH);
 
         for (Language language : englishLanguages) {
-            configure(
-                    context,
-                    language,
-                    "standard",
-                    // char filters:
-                    new String[] { "html_strip" },
-                    // token filters:
-                    new String[] {
-                            // A pattern capture filter to address FQCNs
-                            compoundTechnicalNameFilter(language),
-                            // To make all words in lowercase.
-                            "lowercase",
-                            // To remove possessives (trailing 's) from words.
-                            possessiveStemmerFilter(language),
-                            // To remove frequently used words that do not bring much meaning, e.g. a, that, and, are, as, at, with...
-                            stopFilter(language),
-                            // To remove suffixes like -s/-es/-ed etc
-                            regularStemmerFilter(language),
-                            // To convert characters into ascii ones, e.g. à to a or ę to e etc.
-                            "asciifolding"
-                    },
-                    //  search token filters:
-                    new String[] {
-                            "lowercase",
-                            possessiveStemmerFilter(language),
-                            stopFilter(language),
-                            regularStemmerFilter(language),
-                            "asciifolding",
-                            // > In general, synonym filters rewrite their inputs to the tokenizer and filters used in the preceding analysis chain
-                            // Note how the synonym filter is added in the end. According to https://www.elastic.co/blog/boosting-the-power-of-elasticsearch-with-synonyms
-                            // preceding filters should get applied to the synonyms we passed to it, so we don't need to bother about normalizing them in some way:
-                            synonymsGraphFilter(language)
-                    });
+            configureEnglishLikeLanguage(context, language);
         }
 
         // Japanese
         // https://www.elastic.co/guide/en/elasticsearch/plugins/current/analysis-kuromoji-analyzer.html
-        configure(
-                context,
-                Language.JAPANESE,
-                "kuromoji_tokenizer",
-                // char filters:
-                new String[] {
-                        // If a text contains full-width characters, the kuromoji_tokenizer tokenizer can produce unexpected tokens.
-                        // To avoid this, the icu_normalizer character filter is added.
-                        // Requires icu plugin being installed.
-                        "icu_normalizer",
-                        "html_strip"
-                },
-                // token filters:
-                new String[] {
-                        compoundTechnicalNameFilter(Language.JAPANESE),
-                        "lowercase",
-                        "kuromoji_baseform",
-                        "kuromoji_part_of_speech",
-                        possessiveStemmerFilter(Language.JAPANESE),
-                        "ja_stop",
-                        stopFilter(Language.JAPANESE),
-                        "kuromoji_stemmer",
-                        regularStemmerFilter(Language.JAPANESE),
-                        "asciifolding"
-                },
-                //  search token filters:
-                new String[] {
-                        "lowercase",
-                        "kuromoji_baseform",
-                        "kuromoji_part_of_speech",
-                        possessiveStemmerFilter(Language.JAPANESE),
-                        "ja_stop",
-                        stopFilter(Language.JAPANESE),
-                        "kuromoji_stemmer",
-                        regularStemmerFilter(Language.JAPANESE),
-                        "asciifolding",
-                        synonymsGraphFilter(Language.JAPANESE)
-                });
+        configureJapanese(context);
 
         // Chinese
         // https://www.elastic.co/guide/en/elasticsearch/plugins/current/_reimplementing_and_extending_the_analyzers.html
-        configure(
-                context,
-                Language.CHINESE,
-                "smartcn_tokenizer",
-                // char filters:
-                new String[] { "html_strip" },
-                // token filters:
-                new String[] {
-                        compoundTechnicalNameFilter(Language.CHINESE),
-                        "lowercase",
-                        possessiveStemmerFilter(Language.CHINESE),
-                        "smartcn_stop",
-                        stopFilter(Language.CHINESE),
-                        regularStemmerFilter(Language.CHINESE),
-                        "asciifolding"
-                },
-                //  search token filters:
-                new String[] {
-                        "lowercase",
-                        possessiveStemmerFilter(Language.CHINESE),
-                        stopFilter(Language.CHINESE),
-                        regularStemmerFilter(Language.CHINESE),
-                        "asciifolding",
-                        synonymsGraphFilter(Language.CHINESE),
-                        // must go last as it conflicts wit the synonyms filter.
-                        "smartcn_stop"
-                });
+        configureChinese(context);
     }
 
-    private static void configure(
-            ElasticsearchAnalysisConfigurationContext context,
-            Language language,
-            String tokenizer,
-            String[] charFilters,
-            String[] tokenFilters,
-            String[] searchTokenFilters) {
-
-        configureTokenFilters(context, language);
-
+    void configureEnglishLikeLanguage(ElasticsearchAnalysisConfigurationContext context, Language language) {
+        configureSharedFilters(context, language);
         // The default analyzer to prepare tokens for when the search is performed for the full words.
         // To be applied to the indexed text.
         context.analyzer(defaultAnalyzer(language)).custom()
-                .tokenizer(tokenizer)
-                .tokenFilters(tokenFilters)
-                .charFilters(charFilters);
+                .tokenizer("standard")
+                .tokenFilters(
+                        // A pattern capture filter to address FQCNs
+                        compoundTechnicalNameFilter(language),
+                        // To make all words in lowercase.
+                        "lowercase",
+                        // To remove possessives (trailing 's) from words.
+                        possessiveStemmerFilter(language),
+                        // To remove frequently used words that do not bring much meaning, e.g. a, that, and, are, as, at, with...
+                        stopFilter(language),
+                        // To remove suffixes like -s/-es/-ed etc
+                        regularStemmerFilter(language),
+                        // To convert characters into ascii ones, e.g. à to a or ę to e etc.
+                        "asciifolding")
+                .charFilters("html_strip");
 
         // The analyzer to be applied to the user-input text.
         context.analyzer(defaultSearchAnalyzer(language)).custom()
-                .tokenizer(tokenizer)
-                .tokenFilters(searchTokenFilters)
-                .charFilters(charFilters);
+                .tokenizer("standard")
+                .tokenFilters(
+                        "lowercase",
+                        possessiveStemmerFilter(language),
+                        stopFilter(language),
+                        regularStemmerFilter(language),
+                        "asciifolding",
+                        // > In general, synonym filters rewrite their inputs to the tokenizer and filters used in the preceding analysis chain
+                        // Note how the synonym filter is added in the end. According to https://www.elastic.co/blog/boosting-the-power-of-elasticsearch-with-synonyms
+                        // preceding filters should get applied to the synonyms we passed to it, so we don't need to bother about normalizing them in some way:
+                        synonymsGraphFilter(language))
+                .charFilters("html_strip");
 
         // The autocomplete analyzer to prepare tokens for when the user types the words,
         //  and we want to produce results on incompletely typed (unfinished) words.
         //  To get that we add an edge Ngram filter in the end of the list of filters of the default analyzer
         //  to make sure that we get the same tokens processed into Ngrams.
         //  To be applied to the indexed text.
-        String[] autocompleteTokenFilters = Arrays.copyOf(tokenFilters, tokenFilters.length + 1);
-        autocompleteTokenFilters[tokenFilters.length] = autocompleteEdgeNgramFilter(language);
         context.analyzer(autocompleteAnalyzer(language)).custom()
-                .tokenizer(tokenizer)
-                .tokenFilters(autocompleteTokenFilters)
-                .charFilters(charFilters);
+                .tokenizer("standard")
+                .tokenFilters(
+                        compoundTechnicalNameFilter(language),
+                        "lowercase",
+                        possessiveStemmerFilter(language),
+                        stopFilter(language),
+                        regularStemmerFilter(language),
+                        "asciifolding",
+                        autocompleteEdgeNgramFilter(language))
+                .charFilters("html_strip");
 
         context.normalizer(language.addSuffix(SORT)).custom()
                 .tokenFilters("lowercase");
     }
 
-    private static void configureTokenFilters(ElasticsearchAnalysisConfigurationContext context, Language language) {
+    void configureJapanese(ElasticsearchAnalysisConfigurationContext context) {
+        Language language = Language.JAPANESE;
+        configureSharedFilters(context, language);
+
+        context.analyzer(defaultAnalyzer(language)).custom()
+                .tokenizer("kuromoji_tokenizer")
+                .tokenFilters(
+                        compoundTechnicalNameFilter(language),
+                        "lowercase",
+                        "kuromoji_baseform",
+                        "kuromoji_part_of_speech",
+                        possessiveStemmerFilter(language),
+                        "ja_stop",
+                        stopFilter(language),
+                        "kuromoji_stemmer",
+                        regularStemmerFilter(language),
+                        "asciifolding")
+                .charFilters(
+                        // If a text contains full-width characters, the kuromoji_tokenizer tokenizer can produce unexpected tokens.
+                        // To avoid this, the icu_normalizer character filter is added.
+                        // Requires icu plugin being installed.
+                        "icu_normalizer",
+                        "html_strip");
+
+        context.analyzer(defaultSearchAnalyzer(language)).custom()
+                .tokenizer("kuromoji_tokenizer")
+                .tokenFilters(
+                        "lowercase",
+                        "kuromoji_baseform",
+                        "kuromoji_part_of_speech",
+                        possessiveStemmerFilter(language),
+                        "ja_stop",
+                        stopFilter(language),
+                        "kuromoji_stemmer",
+                        regularStemmerFilter(language),
+                        "asciifolding",
+                        synonymsGraphFilter(language))
+                .charFilters(
+                        "icu_normalizer",
+                        "html_strip");
+
+        context.analyzer(autocompleteAnalyzer(language)).custom()
+                .tokenizer("kuromoji_tokenizer")
+                .tokenFilters(
+                        compoundTechnicalNameFilter(language),
+                        "lowercase",
+                        "kuromoji_baseform",
+                        "kuromoji_part_of_speech",
+                        possessiveStemmerFilter(language),
+                        "ja_stop",
+                        stopFilter(language),
+                        "kuromoji_stemmer",
+                        regularStemmerFilter(language),
+                        "asciifolding",
+                        autocompleteEdgeNgramFilter(language))
+                .charFilters(
+                        "icu_normalizer",
+                        "html_strip");
+
+        context.normalizer(language.addSuffix(SORT)).custom()
+                .tokenFilters("lowercase");
+    }
+
+    void configureChinese(ElasticsearchAnalysisConfigurationContext context) {
+        Language language = Language.CHINESE;
+        configureSharedFilters(context, language);
+        // The default analyzer to prepare tokens for when the search is performed for the full words.
+        // To be applied to the indexed text.
+        context.analyzer(defaultAnalyzer(language)).custom()
+                .tokenizer("smartcn_tokenizer")
+                .tokenFilters(
+                        compoundTechnicalNameFilter(language),
+                        "lowercase",
+                        possessiveStemmerFilter(language),
+                        "smartcn_stop",
+                        stopFilter(language),
+                        regularStemmerFilter(language),
+                        "asciifolding")
+                .charFilters("html_strip");
+
+        // The analyzer to be applied to the user-input text.
+        context.analyzer(defaultSearchAnalyzer(language)).custom()
+                .tokenizer("smartcn_tokenizer")
+                .tokenFilters(
+                        "lowercase",
+                        possessiveStemmerFilter(language),
+                        stopFilter(language),
+                        regularStemmerFilter(language),
+                        "asciifolding",
+                        synonymsGraphFilter(language),
+                        // must go last as it conflicts wit the synonyms filter.
+                        "smartcn_stop")
+                .charFilters("html_strip");
+
+        context.analyzer(autocompleteAnalyzer(language)).custom()
+                .tokenizer("smartcn_tokenizer")
+                .tokenFilters(
+                        compoundTechnicalNameFilter(language),
+                        "lowercase",
+                        possessiveStemmerFilter(language),
+                        "smartcn_stop",
+                        stopFilter(language),
+                        regularStemmerFilter(language),
+                        "asciifolding",
+                        autocompleteEdgeNgramFilter(language))
+                .charFilters("html_strip");
+
+        context.normalizer(language.addSuffix(SORT)).custom()
+                .tokenFilters("lowercase");
+
+    }
+
+    private static void configureSharedFilters(ElasticsearchAnalysisConfigurationContext context, Language language) {
         context.tokenFilter(stopFilter(language))
                 .type("stop")
                 .param("stopwords", "_english_")
